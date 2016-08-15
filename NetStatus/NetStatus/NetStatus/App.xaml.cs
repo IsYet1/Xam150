9b7c7d52d9b35c8a4834b905e605302398657bb0
IsYet1 <Don@IsYet.com>
2016-08-15 18:45:50 -0500

    Added code to check dynamically.

--- NetStatus/NetStatus/NetStatus/App.xaml.cs
+++ NetStatus/NetStatus/NetStatus/App.xaml.cs
@@ -14,12 +14,24 @@ namespace NetStatus
         public App()
         {
             InitializeComponent();
-            //var isConnected = CrossConnectivity.Current.IsConnected;
-            //MainPage = new NoNetworkPage();
-            //MainPage = new NetworkViewPage();
             MainPage = (CrossConnectivity.Current.IsConnected)
                 ? (Page)new NetworkViewPage()
                 : new NoNetworkPage();
+        }
+
+        protected override void OnStart()
+        {
+            base.OnStart();
+            CrossConnectivity.Current.ConnectivityChanged += HandleConnectivityChanged;
+        }
+
+        private void HandleConnectivityChanged(object sender, Plugin.Connectivity.Abstractions.ConnectivityChangedEventArgs e)
+        {
+            Type currentPage = this.MainPage.GetType();
+            if (e.IsConnected && currentPage != typeof(NetworkViewPage))
+                this.MainPage = new NetworkViewPage();
+            else if (!e.IsConnected && currentPage != typeof(NoNetworkPage))
+                this.MainPage = new NoNetworkPage();
         }
     }
 }
