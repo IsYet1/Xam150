af3b67003fb17a7e2c6e93cbcdf516e5d15f39d5
IsYet1 <Don@IsYet.com>
2016-08-17 16:01:03 -0500

    Added login code. Not tested yet.

--- BookClient/BookClient/BookClient/Data/BookManager.cs
+++ BookClient/BookClient/BookClient/Data/BookManager.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -7,6 +9,23 @@ namespace BookClient.Data
 {
     public class BookManager
     {
+        const string Url = "http://xam150.azurewebsites.net/api/books/";
+        private string authorizationKey;
+
+        private async Task<HttpClient> GetClient()
+        {
+            var client = new HttpClient();
+            if (!String.IsNullOrEmpty(authorizationKey))
+            {
+                authorizationKey = await client.GetStringAsync(Url + "login");
+                authorizationKey = JsonConvert.DeserializeObject<String>(authorizationKey);
+            }
+
+            client.DefaultRequestHeaders.Add("Authorization", authorizationKey);
+            client.DefaultRequestHeaders.Add("Accept", "application/json");
+            return client;
+        }
+
         public Task<IEnumerable<Book>> GetAll()
         {
             // TODO: use GET to retrieve books
